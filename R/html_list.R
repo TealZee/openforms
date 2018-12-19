e8a10f74bf261bf279fd1dd37e1141114403964d
TealZee <37114970+TealZee@users.noreply.github.com>
2018-12-19 12:33:51 -0500

    Added html_list function
    html_list takes a single-column dataframe or a list and formats its values into a bulleted list in html code.

---
+++ R/html_list.R
@@ -0,0 +1,37 @@
+#' Format input items into a html bulleted list
+#'
+#' This function formats items into a html bulleted list
+#' @param items List or dataframe of items to be formatted
+#' @export
+#' @examples
+#' of_import(1000, apiKey)
+
+html_list <- function (items) {
+
+  # DETERMINE DATA TYPE OF ITEMS
+  if (is.data.frame(items)) {
+    itemsCount <- nrow(items)
+  } else {
+    itemsCount <- length(items)
+  }
+
+  # FORMAT ITEMS INTO HTML BULLETED LIST
+  if (itemsCount > 0) {
+    list <- paste('<ul><li>', items[1], '</li>', sep="")
+  }
+
+  if (itemsCount > 1) {
+    i <- 2
+
+    for (i in i:itemsCount) {
+      list <- paste(list, '<li>', items[i], '</li>', sep="")
+      i <- i + 1
+    }
+  }
+
+  if (itemsCount > 0) {
+    list <- paste(list, '</ul>', sep="")
+  }
+
+  return(list)
+}
